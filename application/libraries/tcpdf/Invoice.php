90f70f4ba0de23739710db4903e57bf25f0742f2
Kava Developers <mehul9921@gmail.com>
2020-08-02 19:42:23 +0530

    Update Invoice.php

--- application/libraries/tcpdf/Invoice.php
+++ application/libraries/tcpdf/Invoice.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-require_once dirname(__FILE__) . '/tcpdf.php';
+require_once dirname(__FILE__) . '/Tcpdf.php';
 
 class Invoice extends TCPDF
 {
